77d209ea845897e6f5e22144c26dd470662d8851
liuzongjie <liuzongj@newland.com.cn>
2019-01-26 14:38:34 +0800

    [test] bantch

--- src/main/java/com/service/service/WebServiceClient.java
+++ src/main/java/com/service/service/WebServiceClient.java
@@ -4,11 +4,12 @@ import org.apache.cxf.frontend.ClientProxyFactoryBean;
 import org.apache.cxf.jaxws.JaxWsClientFactoryBean;
 
 public class WebServiceClient {
+
     public static void main(String[] args){
         ClientProxyFactoryBean factory = new ClientProxyFactoryBean(new JaxWsClientFactoryBean());
         factory.setAddress("http://127.0.0.1:8989/service");
         factory.setServiceClass(IWebService.class);
-        IWebService service = (IWebService) factory.create();
+        IWebService service = (IWebService)factory.create();
         String rsp1 = service.sayHello();
         System.out.println("rs12p:" + rsp1);
     }
